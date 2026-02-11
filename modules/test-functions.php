90295f2421bd90e7392dd14c0090477b4caa404d
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-11 18:18:41 +0100

    Working more on functions now

--- modules/test-functions.php
+++ modules/test-functions.php
@@ -1,18 +1,19 @@
 <?php
-// Testing Logic ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Basic testing function
-function test ($title, $case, $function, $to_equal) {
-    $_SESSION['tests']++;
+function test_title ($title) {
     echo '<div class="container">' . "<h3>Function: " . $title . '</h3></div>';
-    echo '<div class="container">' . "<h3>Case: " . $case . '</h3></div>';
+}
 
-    if ($function === $to_equal) {
-        echo '<div class="container">' . '<p>Passed</p><br>' . '</div>';
+function test ($case, $function_return, $to_equal) {
+    $_SESSION['tests']++;
+    echo '<div class="container"><h3 style="margin-left: 20px; margin-top: 20px;">Test Case: ' . $case . '</h3></div>';
+
+    if ($function_return === $to_equal) {
+        echo '<div class="container"><h4 style="margin-left: 20px">Passed &#10004</h4></div>';
 
     } else {
         $_SESSION['failures']++;
-        echo '<div class="container">' . '<p>Failed</p><br>' . '</div>';
+        echo '<div class="container"><h4 style="margin-left: 20px; color: #ba0505;">Failed &#10008</h4></div>';
+        echo "<div class='container'><h4 style='margin-left: 20px; color: #ba0505;'>Expected '$function_return' to equal '$to_equal'.</h4></div>";
     }
 }
-// Testing Logic ////////////////////////////////////////////////////////////////////////////////////////////////////
--- modules/test.php
+++ modules/test.php
@@ -4,14 +4,17 @@ require_once './modules/test-functions.php';
 require_once './classes/Test.php';
 
 // Trying out basic testing function
-// test('sabiranje', 'Normalno sabiranje', sabiranje(5, 5), 10);
-// test('sabiranje', 'Sabiranje sa nulom', sabiranje(5, 0), 5);
-// test('sabiranje', 'Sabiranje sa brojevima ispod nule', sabiranje(-4, 4), 0);
+test_title('sabiranje');
+test('Normalno sabiranje', sabiranje(5, 5), 10);
+test('Sabiranje sa nulom', sabiranje(5, 0), 5);
+test('Sabiranje sa brojevima ispod nule', sabiranje(-4, 4), 0);
+test('Sabiranje sa negativnim brojevima', sabiranje(-4, -4), -8);
 
 // Trying out class testing functions
-$test_for_sabiranje = new Test('sabiranje()');
-$test_for_sabiranje->title();
-$test_for_sabiranje->assert('normalno sabiranje', sabiranje(5, 5), 10);
-$test_for_sabiranje->assert('sabiranje sa nulom', sabiranje(4, 0), 4);
-$test_for_sabiranje->assert('sabiranje sa negativnim brojevima', sabiranje(-4, 4), 0);
-$test_for_sabiranje->assert('sabiranje sa dva negativna broja', sabiranje(-4, -4), -8);
+// $test_for_sabiranje = new Test('sabiranje()');
+// $test_for_sabiranje->title();
+// $test_for_sabiranje->assert('normalno sabiranje', sabiranje(5, 5), 10);
+// $test_for_sabiranje->assert('sabiranje sa nulom', sabiranje(4, 0), 4);
+// $test_for_sabiranje->assert('sabiranje sa negativnim brojevima', sabiranje(-4, 4), 0);
+// $test_for_sabiranje->assert('sabiranje sa dva negativna broja', sabiranje(-4, -4), -8);
+// $test_for_sabiranje->assert('sabiranje sa brojevima iznad 1000', sabiranje(1001, 5000), 6002);
