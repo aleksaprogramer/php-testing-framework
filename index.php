a59c99798f1b8ddd28187df5d32be12fd16bc9e1
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-13 12:39:18 +0100

    Changing directories

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $_SESSION['tests'] = 0;
 $_SESSION['failures'] = 0;
 
 require_once './inc/header.php';
-require_once './modules/test.php';
+require_once './specs/test.php';
 require_once './inc/results.php';
 require_once './inc/warning-line.php';
 require_once './inc/footer.php';
