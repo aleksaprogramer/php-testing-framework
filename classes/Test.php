c47188093df8df2968fc5b3a69fee0be55b023fe
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-13 12:56:45 +0100

    Working with classes

--- classes/Test.php
+++ classes/Test.php
@@ -2,34 +2,40 @@
 
 class Test {
     public $title;
+    public $case;
+    public $function;
 
     public function __construct ($title) {
         $this->title = $title;
     }
 
-    public function get_title () {
-        return $this->title;
+    public function test_case ($case) {
+        $this->case = $case;
+        return $this;
     }
 
-    public function set_title ($title) {
-        $this->title = $title;
+    public function expect ($function) {
+        $this->function = $function;
+        return $this;
     }
 
-    public function title () {
-        echo '<div class="container">' . "<h4>Function: " . $this->title . "</h4><br>" . '</div>';
-    }
-
-    public function assert ($case, $function_return, $equal_to) {
+    public function to_equal ($expected_result) {
         $_SESSION['tests']++;
-        echo '<div class="container">' . "Test Case: " . $case . "<br>" . '</div>';
+        echo '<div class="container"><h3 style="margin-left: 20px; margin-top: 20px;">Test Case: ' . $this->case . '</h3></div>';
 
-        if ($function_return === $equal_to) {
-            echo '<div class="container">' . "<p>Passed &#10004</p>" . "<br><br>" . '</div>';
+        if ($this->function === $expected_result) {
+            echo '<div class="container"><h4 style="margin-left: 20px; color: #03991f;">Passed &#10004</h4></div>';
 
         } else {
             $_SESSION['failures']++;
-            echo '<div class="container">' . "<p>Failed &#10008</p>" . "<br>" . '</div>';
-            echo '<div class="container">' . "<p>Expected $function_return to equal $equal_to.</p>" . "<br>" . '</div>';
+            echo '<div class="container"><h4 style="margin-left: 20px; color: #ba0505;">Failed &#10008</h4></div>';
+            echo "<div class='container'><h4 style='margin-left: 20px; color: #ba0505;'>Expected '$this->function' to equal '$expected_result'.</h4></div>";
         }
     }
+}
+
+function testing ($title) {
+    $new_test = new Test($title);
+    echo "<div class='container'><hr style='margin-top: 30px;'><h3 style='margin-top: 10px;'>Function: $title()</h3></div>";
+    return $new_test;
 }
