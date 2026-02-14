0dc8da6af37998687b1bd8aa0e9829947e28320c
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-14 16:44:52 +0100

    Adding comments

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ $_SESSION['tests'] = 0;
 $_SESSION['failures'] = 0;
 
 require_once './lib/inc/header.php';
+
+// Test files go here
 require_once './specs/additionTest.php';
+// Test files go here ^^^
+
 require_once './lib/inc/results.php';
 require_once './lib/inc/warning-line.php';
 require_once './lib/inc/footer.php';
