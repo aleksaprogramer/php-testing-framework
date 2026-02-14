b94d3b21f962ebbd2ea5086284fe06dd4ff6c937
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-14 17:03:07 +0100

    Small chage

--- specs/additionTest.php
+++ specs/additionTest.php
@@ -8,4 +8,4 @@ test_case('Standard addition')->expect(addition(2, 3))->to_equal('5');
 test_case('Addition with 0')->expect(addition(3, 0))->to_equal('3');
 test_case('Addition with negative number')->expect(addition(3, -3))->to_equal('0');
 test_case('Addition with both negative numbers')->expect(addition(-4, -5))->to_equal('-9');
-test_case('Addition with decimal numbers')->expect(addition(0.54, 0.33))->to_equal('0.88');
+test_case('Addition with decimal numbers')->expect(addition(0.54, 0.33))->to_equal('0.87');
